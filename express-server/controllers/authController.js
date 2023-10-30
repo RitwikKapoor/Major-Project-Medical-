7e32bbc208804107b3a693c7039282b70365be13
RitwikKapoor <kapoorritwik2020@gmail.com>
2023-10-30 13:06:15 +0530

    starter code

---
+++ express-server/controllers/authController.js
@@ -0,0 +1,10 @@
+export const register = async (req, res) => {
+  const { email, password, name, role, photo, gender, dob } = req.body;
+
+  try {
+  } catch (err) {
+    
+  }
+};
+
+export const login = () => {};
---
+++ express-server/routes/authRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.post("/register",register);
+// router.post("/login", login);
+
+export { router as  authRoute };
