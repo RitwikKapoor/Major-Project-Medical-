b42d3abac976640cf76b42dfd003291e0d908620
RitwikKapoor <kapoorritwik2020@gmail.com>
2024-01-23 18:51:14 +0530

    routes made better

--- client/src/utils/route.jsx
+++ client/src/utils/route.jsx
@@ -1,7 +1,6 @@
 import { Navigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
 export const Public = ({ children }) => {
   const isLoggedIn = localStorage.getItem("isLoggedIn");
@@ -24,131 +23,66 @@ export const Protected = ({ children }) => {
 };
 
 export const Admin = ({ children }) => {
-  const [isAdmin, setIsAdmin] = useState(false);
-  const [redirect, setRedirect] = useState(null);
+  const { role } = useSelector((state) => state.root.user);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (role) {
+      setLoading(false);
+    }
+  }, [role]);
 
-        if (!isLoggedIn) {
-          setRedirect("/login");
-          return;
-        }
-
-        const response = await axios.get(
-          `${import.meta.env.VITE_APP_BASE_URL}/user/getUserInfo`,
-          {
-            withCredentials: true,
-          }
-        );
-
-        if (response.status === 200 && response.data.isAdmin) {
-          setIsAdmin(true);
-        } else {
-          setIsAdmin(false);
-          setRedirect("/home");
-        }
-      } catch (error) {
-        toast.error("Admin Auth Failed");
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (redirect) {
-    return <Navigate to={redirect} replace={true} />;
+  if (loading) {
+    return null;
   }
 
-  return isAdmin ? children : null;
+  if (role === "admin") {
+    return children;
+  }
+
+  return <Navigate to="/login" replace={true} />;
 };
 
 export const Doctor = ({ children }) => {
-  const [isDoctor, setIsDoctor] = useState(false);
-  const [redirect, setRedirect] = useState(null);
+  const { role } = useSelector((state) => state.root.user);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (role) {
+      setLoading(false);
+    }
+  }, [role]);
 
-        if (!isLoggedIn) {
-          setRedirect("/login");
-          return;
-        }
-
-        const response = await axios.get(
-          `${import.meta.env.VITE_APP_BASE_URL}/user/getUserInfo`,
-          {
-            withCredentials: true,
-          }
-        );
-
-        if (response.status === 200 && response.data.isDoctor) {
-          setIsDoctor(true);
-        } else {
-          setIsDoctor(false);
-          setRedirect("/home");
-        }
-      } catch (error) {
-        toast.error("Doctor Auth Failed");
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (redirect) {
-    return <Navigate to={redirect} replace={true} />;
+  if (loading) {
+    return null;
   }
 
-  return isDoctor ? children : null;
+  if (role === "doc") {
+    return children;
+  }
+
+  return <Navigate to="/login" replace={true} />;
 };
 
 export const User = ({ children }) => {
-  const [isUser, setIsUser] = useState(false);
-  const [redirect, setRedirect] = useState(null);
+  const { role } = useSelector((state) => state.root.user);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (role) {
+      setLoading(false);
+    }
+  }, [role]);
 
-        if (!isLoggedIn) {
-          setRedirect("/login");
-          return;
-        }
-
-        const response = await axios.get(
-          `${import.meta.env.VITE_APP_BASE_URL}/user/getUserInfo`,
-          {
-            withCredentials: true,
-          }
-        );
-
-        if (
-          response.status === 200 &&
-          !response.data.isDoctor &&
-          !response.data.isAdmin
-        ) {
-          setIsUser(true);
-        } else {
-          setIsUser(false);
-          setRedirect("/home");
-        }
-      } catch (error) {
-        toast.error("User Auth Failed");
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (redirect) {
-    return <Navigate to={redirect} replace={true} />;
+  if (loading) {
+    return null;
   }
 
-  return isUser ? children : null;
+  if (role === "user") {
+    return children;
+  }
+
+  return <Navigate to="/login" replace={true} />;
 };
+
+
