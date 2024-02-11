33f35d9fba7b4a0a20b4d23e8f54c862f3c6ecf4
Ritwik Kapoor <89243656+RitwikKapoor@users.noreply.github.com>
2024-02-11 13:00:44 +0530

    Update index.js

--- express-server/index.js
+++ express-server/index.js
@@ -32,6 +32,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+     process.env.FRONTEND_URL,
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
